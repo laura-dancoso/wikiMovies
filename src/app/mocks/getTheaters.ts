1d453abff1a44cb0a5ca588cba51471510f26cf0
laura-dancoso <lauradancoso@gmail.com>
2023-06-10 18:11:04 -0300

    add mocks

---
+++ src/app/mocks/getTheaters.ts
@@ -0,0 +1,34 @@
+import { Theater } from "../shared/theaters/models/theater.model";
+
+export const THEATERS: Theater[] = [
+    {
+        id: 1,
+        description: 'Cinemark Palermo',
+        url: "https://www.cinemarkhoyts.com.ar"
+    },
+    {
+        id: 2,
+        description: 'Cinemark Belgrano',
+        url: "https://www.cinemarkhoyts.com.ar"
+    },
+    {
+        id: 3,
+        description: 'Cinepolis Recoleta',
+        url: "https://www.cinepolis.com.ar/"
+    },
+    {
+        id: 4,
+        description: 'Atlas Caballito',
+        url: "https://www.atlascines.com/"
+    },
+    {
+        id: 5,
+        description: 'Multiplex Belgrano',
+        url: "https://www.multiplex.com.ar/complejos/multiplex-belgrano/"
+    },
+    {
+        id: 6,
+        description: 'Multiplex Lavalle',
+        url: "https://www.multiplex.com.ar/complejos/multiplex-lavalle/"
+    }    
+];
