773ecc0a2d645ddd7673eb493ebf2b8aa4888f37
Brisa Sardón <sardonbrisa21@gmail.com>
2023-06-28 19:37:30 -0300

    agregando search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SearchMovieComponent } from './movies/components/search-movie/search-movie.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SearchMovieComponent ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  exports: [SearchMovieComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/movies/components/search-movie/search-movie.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-movie',
+  templateUrl: './search-movie.component.html',
+  styleUrls: ['./search-movie.component.scss'],
+})
+export class SearchMovieComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
