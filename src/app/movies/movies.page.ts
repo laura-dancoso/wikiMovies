77ccfde58978ea60f7750eb45c232610851b7cb3
laura-dancoso <lauradancoso@gmail.com>
2023-07-01 19:15:25 -0300

    update implementation

--- src/app/movies/movies.page.ts
+++ src/app/movies/movies.page.ts
@@ -1,10 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NameTabs } from '../enums/tabs-enum';
-import { MoviesService } from './services/movies.service';
-import { GenresService } from '../shared/genres/services/genres.service';
-import { TheatersService } from '../shared/theaters/services/theaters.service';
-import { MoviesListComponent } from './components/movies-list/movies-list.component';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'movies',
@@ -13,6 +8,6 @@ import { environment } from 'src/environments/environment';
 })
 export class MoviesPage {
   
-  title = `Prod: ${environment.production}`;
+  title = NameTabs.Movies;
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: ''
+  api: 'https://wiki-movies-api.vercel.app/api'
 };
