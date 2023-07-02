eb92ccf1c976900d6197c824c7701f1e42a81531
laura-dancoso <lauradancoso@gmail.com>
2023-07-02 10:29:03 -0300

    Merge branch 'main' into api

--- src/app/movies/components/movie-detail/movie-detail.component.ts
+++ src/app/movies/components/movie-detail/movie-detail.component.ts
@@ -3,6 +3,7 @@ import { MovieDetail } from '../../models/movie.model';
 import { MoviesService } from '../../services/movies.service';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { Genre } from 'src/app/shared/genres/models/genre.model';
 
 @Component({
   selector: 'movie-detail',
@@ -28,5 +29,9 @@ export class MovieDetailComponent  implements OnInit {
   handleRefresh(event: any){
     this.movie$ = this.moviesService.getMovie(this.id);
     event.target.complete();
+  }
+
+  getGenres(genres: Genre[]){
+    return genres.map(genres=> genres.description).join(', ');
   }
 }
